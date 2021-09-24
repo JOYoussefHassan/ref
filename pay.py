e4acd1c4b4a4d45a8a111ae698861ca22912b577
يوسف <89800072+JO101100@users.noreply.github.com>
2021-09-24 14:11:58 +0200

    Update pay.py
    add password method

--- pay.py
+++ pay.py
@@ -5,10 +5,10 @@ _pth = str(os.path.dirname(os.path.abspath(__file__))) + "/log.txt"
 print(("#" * 100) + "\n" + ("#" + "Welcome to PAYMENT_TABLE.JO logging".center(98) + "#") + "\n" + ("#" * 100))
 _doc = "# Please use these commands \\ Add (a), Remove (r), Show (s), Help (h) or Exit (e)"
 print(_doc)
-# process
-while True :
-    # input
-    _cmd = input("PAYMENT_TABLE.JO> ").split(" ", 2)
+## password
+_rot = "760320"
+## function
+def _pay(_cmd, _pass) :
     # file
     _flw = open(_pth, "a")
     _flr = open(_pth, "r")
@@ -36,41 +36,42 @@ while True :
             except :
                 _flw.write("\n")
             _flw.close()
+            # ensure added
             print("# Successfully added")
         except :
             print("# Wrong syntax\n# The syntax is : a [s]<number> [discription]")
     ## remove
     elif _cmd[0].lower() in ["remove", "r"] :
         try :
+            # data
             _txt = _flr.readlines()
+            ## out of range
             if len(_txt) - 1 < int(_cmd[1]) or int(_cmd[1]) <= 0 :
                 print("# Out of range")
-                continue
-            _flc = open(_pth, "w")
-            _txt.pop(int(_cmd[1]))
-            _txt = "".join(_txt)
-            _flc.write("")
-            _flc.close()
-            _flw.write(_txt)
-            _flw.close()
-            print("# Successfully removed")
+            else :
+                _flc = open(_pth, "w")
+                _txt.pop(int(_cmd[1]))
+                _txt = "".join(_txt)
+                _flc.write("")
+                _flc.close()
+                _flw.write(_txt)
+                _flw.close()
+                # ensure removed
+                print("# Successfully removed")
         except :
             print("# Wrong syntax\n# The syntax is : r <indexNumber>")
     ## show
     elif _cmd[0].lower() in ["show", "s"] :
+        # main of table
         print("-" * 100 + "\n|" + "DATA".center(98) + "|\n" + "-" * 100 + "\n| No. | Type  | " + "Date".ljust(22) + "| " + "Cost".ljust(10) + " | Discription".ljust(49) + "|\n" + "-" * 100)
         x = 1
+        # cells of table
         for i in _flr.readlines()[1:] :
             _dat = i.split(" ==> ")
             print(f"| {str(x).zfill(3)} | {_dat[0]} | {_dat[1]} | {_dat[2].ljust(10)} | {_dat[3]}", end = "")
             x += 1
             print("-" * 100)
         _flw.close()
-    ## exit
-    elif _cmd[0].lower() in ["exit", "e"] :
-        print("# Goodbye :)\n\n© 2021 PAYMENT_TABLE.JO v1.2.2\nPowerd by JO")
-        _flr.close()
-        break
     ## help
     elif _cmd[0].lower() in ["help", "h"] :
         print(_doc)
@@ -78,3 +79,19 @@ while True :
     elif _cmd[0].lower() != "" :
         print("# Wrong syntax\n# The syntax is : Add (a), Remove (r), Show (s), Help (h), or Exit (e)")
     _flr.close()
+## login
+while True :
+    _pas = input("Password> ")
+    if _pas == _rot :
+        # ensure login
+        print("# Successfully login")
+        # input
+        _cmd = input("PAYMENT_TABLE.JO> ").split(" ", 2)
+        ## exit
+        if _cmd[0].lower() in ["exit", "e"] :
+            print("# Goodbye :)\n\n© 2021 PAYMENT_TABLE.JO v1.2.2\nPowerd by JO")
+            break
+        _pay(_cmd, _pas)
+    else :
+        # wrong
+        print("# Wrong password")
