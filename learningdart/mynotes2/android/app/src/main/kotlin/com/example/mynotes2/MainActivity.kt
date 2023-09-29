a152f9fe73bccf425002b636ab6875d26816ba68
يوسف <89800072+hshhshsjsgeh@users.noreply.github.com>
2023-09-29 19:24:33 +0300

    ref

--- learningdart/mynotes2/android/app/src/main/kotlin/com/example/mynotes2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mynotes2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mynotes/android/app/src/main/kotlin/com/mynotesjo/mynotes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mynotesjo.mynotes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
