71e45489ef94c39c1409f42ebe61e5a70fb9a18a
يوسف <89800072+hshhshsjsgeh@users.noreply.github.com>
2023-09-30 11:30:19 +0300

    Update dart.dart

--- dart.dart
+++ dart.dart
@@ -1,15 +1,24 @@
-void main() {
-  int s = 1
-    ..isOdd
-    ..toString()
-    ..isNaN;
+class Todo {
+  final String who;
+  final String what;
 
-  print(s);
+  const Todo(this.who, this.what);
+}
+
+class ClassName {
+  @Todo('Dash', 'Implement this function')
+  void doSomething() {
+    print('Do something');
+  }
+}
+
+void main() {
+  print(1);
 }
 
 /*
 void main() {
-  _statement_;
+  _statement_
   ...
 }
 +--------------------------------+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -140,42 +149,42 @@ _varDeclare_ _varName_ = _data_;
 +----------------------------+
 [1] - _condition_
   [1] - if (_exp_) {
-          _statement_;
+          _statement_
           ...
         } else if () {
-          _statement_;
+          _statement_
           ...
         } else {
-          _statement_;
+          _statement_
           ...
         }
-  [2] - (_exp_) ? _statement_ : _statement_;
+  [2] - (_exp_) ? _statement_ : _statement_
 
 [2] - _loop_
   [1] - _forLoop_
     [1] - for (_varDeclare_ _varName_ in _iterable_) {
-            _statement_;
+            _statement_
             ...
           }
     [2] - for (_varDeclare_ _varName_ = _intStart_; _conditionEnd_; _step_) {
-            _statement_;
+            _statement_
             ...
           }
   [2] - _whileLoop_
     [1] - while (_conditionEnd_) {
-            _statement_;
+            _statement_
             ...
           }
 
 [3] - _exception_
   [1] - try {
-          _statement_;
+          _statement_
           ...
         } on _error_ catch (e) {
-          _statement_;
+          _statement_
           ...
         } finally {
-          _statement_;
+          _statement_
           ...
         }
 +-------------------------+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -184,7 +193,7 @@ _varDeclare_ _varName_ = _data_;
 [1] - _simpleFunction_
   [1] - _blockSimpleFunction_
     [1] - _varDeclare_ _blockSimpleFunctionName_(_varDeclare_ _varName_, ...) {
-            _statement_;
+            _statement_
             ...
             _statementControler_;
           }
@@ -196,15 +205,36 @@ _varDeclare_ _varName_ = _data_;
 | _statement_: _class_ |
 +----------------------+
 [1] - _normalClass_
-  [1] - 
+  [1] - class _normalClassName_ {
+          @_metadata_
+          _statement_
+          ...
+        }
++-----------------------------+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+| _statement_: _classControl_ |
++-----------------------------+
+[1] - _metadata_
+  [1] - deprecated
+  [2] - Deprecated(_data_)
+  [3] - override
+  [4] - pragma
+  [5] - _className_(_data_, ...)
 +------------------------+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 | _statement_: _library_ |
 +------------------------+
-import '_importingFunction_:_path_'
+@_className_(_data_,...)                                                                                    ===> to test library
+library;                                                                                                    ===> to set it a library
+import '_importingFunction_:_path_' _libraryControl_;
 
 [1] - _importingFunction_
   [1] - 
   [2] - dart
   [3] - package
+[2] - _libraryControl_
+  [1] - as _libraryName_
+  [2] - show _object_instance_
+  [3] - hide _object_instance_
+  [4] - deferred as _libraryName_
+        await _libraryName_.loadLibrary();                                                                  ===> to load lazily library
 +------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 */
