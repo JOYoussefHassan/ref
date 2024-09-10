eada2ae339980d6d531fea425d372877458ceecc
hdgdgsmdj <89800072+hshhshsjsgeh@users.noreply.github.com>
2024-09-10 23:45:17 +0300

    Update dart.dart

--- dart.dart
+++ dart.dart
@@ -80,7 +80,7 @@ _varDeclare_ (_varName_, ...) = (_data_, ...);                                  
     [17] - _genericDatatype_                                                                                                                        ===> in class as <T> to allow pass of any data
   [4] - void
   [5] - static
-        in class only can be accessed without creating instance class (new class inside variable)
+        in class only can be accessed without creating instance class (new class inside variable), and edit class data
 +--------------------+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 | _statement_: _exp_ |
 +--------------------+
