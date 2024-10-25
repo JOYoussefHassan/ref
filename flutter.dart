0225b07e4f526903b2beaa10f9468c1d3bc377e2
hdgdgsmdj <89800072+hshhshsjsgeh@users.noreply.github.com>
2024-10-25 16:05:41 +0300

    Update flutter.dart

--- flutter.dart
+++ flutter.dart
@@ -51,8 +51,8 @@ flutter:
 +-----------------------+
 --- Text(
       '_data_',
-      style: _TextStyle_,
       key: _key_,
+      style: _TextStyle_,
     )
 --- Container(
       key: _key_,
@@ -73,20 +73,52 @@ flutter:
         _widget_,
       ],
     )
+
+--- OverflowBox(
+      key: _key_,
+      minWidth: _double_,
+      minHeight: _double_,
+      maxWidth: _double_,
+      maxHeight: _double_,
+      child: _widget_,
+    )
+
+--- LimitedBox(
+      key: _key_,
+      maxWidth: _double_,
+      maxHeight: _double_,
+      child: _widget_,
+    )
+--- FittedBox(
+      key: _key_,
+      child: _widget_,
+    )
+
+--- ConstrainedBox(
+      key: _key_,
+      constraints: _BoxConstraints_,                                                                                 ===> _BoxConstraints_ (nothing, loose, tight)
+      child: _widget_,
+    )
+--- UnconstrainedBox(                                                                                                ===> to be the same size of screen
+      key: _key_,
+      child: _widget_,
+    )
 +------------------------+--------------------------------------------------------------------------------------------------------------------------------------------------
 | widget: _widgetLayout_ |
 +------------------------+
 --- Row(
       key: _key_,
-      alignment: _Alignment,
+      alignment: _Alignment_,
       mainAxisAlignment: _MainAxisAlignment_,
+      crossAxisAlignment: _CrossAxisAlignment_,
       mainAxisSize: _MainAxisSize_,
       children: [_widget_, ...],
     )
 --- Column(
       key: _key_,
-      alignment: _Alignment,
+      alignment: _Alignment_,
       mainAxisAlignment: _MainAxisAlignment_,
+      crossAxisAlignment: _CrossAxisAlignment_,
       mainAxisSize: _MainAxisSize_,
       children: [_widget_, ...],
     )
@@ -95,34 +127,64 @@ flutter:
       flex: _int_,
       child: _widget_,
     )
+--- Flexible(
+      key: _key_,
+      child: _widget_,
+    )
+--- Align(
+      key: _key_,
+      alignment: _Alignment_,
+      child: _widget_,
+    )
 
 --- Stack(
+      key: _key_,
       alignment: _Alignment,
       children: [_widget_, ...],
     )
 
 --- SizedBox(
+      key: _key_,
       height: _double_,
       width: _double_,
       child: _widget_,
     )
+--- SizedBox.expand(
+      key: _key_,
+      child: _widget_,
+    )
 
 --- Card(
+      key: _key_,
       child: _widget_,
     )
 
 --- GridView.extent(
+      key: _key_,
       padding: _EdgeInsets_,
       maxCrossAxisExtent: _double_,
       mainAxisSpacing: _double_,
       crossAxisSpacing: _double_,
       children: [_widget_, ...],
     )
-
---- ListView(
+--- GridView.count(
+      key: _key_,
+      crossAxisCount: _int_,
       children: [_widget_, ...],
     )
+
+--- ListView(
+      key: _key_,
+      scrollDirection: _Axis_,
+      children: [_widget_, ...],
+    )
+--- ListView.builder(
+      key: _key_,
+      itemCount: _int_,
+      itemBuilder: (context, index) => _widget_,
+    )
 --- ListTile(
+      key: _key_,
       title: _widgetText_,
       subtitle: _widgetText_,
       leading: _widgetIcon_,
