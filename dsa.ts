c6aa59ca6e103fb55019ea092da4fadd1ef50727
Youssef <89800072+JOYoussefHassan@users.noreply.github.com>
2025-03-05 17:39:05 +0200

    Update dsa.ts

--- dsa.ts
+++ dsa.ts
@@ -1,1 +1,4 @@
-
+function linearLowestNumber(array: number[], data?: number, i: number = 0): number {
+  data = (data == undefined) ? array[0] : data;
+  return (i >= array.length) ? data : lowestNumber(array, (array[i] < data) ?  array[i] : data, ++i);
+}
