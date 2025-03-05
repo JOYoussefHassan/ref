0e2aea7fa9dd06841be708ad413910300fe21107
Youssef <89800072+JOYoussefHassan@users.noreply.github.com>
2025-03-05 19:14:49 +0200

    Update dsa.ts

--- dsa.ts
+++ dsa.ts
@@ -2,3 +2,26 @@ function linearLowestNumber(array: number[], data?: number, i: number = 0): numb
   data = (data == undefined) ? array[0] : data;
   return (i >= array.length) ? data : lowestNumber(array, (array[i] < data) ?  array[i] : data, ++i);
 }
+// --------------------------------------------
+function * swapBubbleSort(first: number, second: number, reverse: boolean = false): Generator<number> {
+  if (first < second) {
+    yield first;
+    yield second;
+  } else {
+    yield second;
+    yield first;
+  }
+}
+function bubbleSort(array: number[], index?: number): number[] {
+  if (index == undefined) {
+    index = array.length - 1;
+  }
+  
+  for (let i: number = 0; i <= index - 1; i++) {
+    let swap: Generator<number> = swapBubbleSort(array[i], array[i + 1]);
+    array[i] = swap.next().value;
+    array[i + 1] = swap.next().value;
+  }
+
+  return (index <= 0) ? array : bubbleSort(array, --index);
+}
